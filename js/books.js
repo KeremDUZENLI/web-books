5a553903f7ab7c644d6175b9208e56c928967fda
KeremDUZENLI <krmdznl97@gmail.com>
2025-12-08 16:12:08 +0100

    build: before slider

--- js/books.js
+++ js/books.js
@@ -1,6 +1,12 @@
-function firstPageChapter() {
-  document.getElementById("container_abstract").innerHTML =
-    "<h1>" + document.title + "</h1>";
+function resetPage() {
+  var containerAbstract = document.getElementById("container_abstract");
+  var containerExercises = document.getElementById("container_exercises");
+  containerExercises.innerHTML = `
+    <div id="panel_questions"></div>
+    <div id="panel_responses"></div>
+  `;
+
+  containerAbstract.innerHTML = "<h1>" + document.title + "</h1>";
 }
 
 function createLinkChapter(chapter) {
@@ -21,77 +27,85 @@ function createLinkChapter(chapter) {
   return link;
 }
 
+function createElement(text, type, container, insertBefore) {
+  var header = document.createElement(type);
+  header.textContent = text;
+  container.insertBefore(header, insertBefore);
+}
+
+function renderAbstract(abstract, containerAbstract) {
+  fetchText(abstract, function (text) {
+    containerAbstract.innerHTML = marked(text);
+  });
+}
+
+function renderExercise(exercise, panelQuestions, panelResponses) {
+  fetchText(exercise.question, function (text) {
+    var divQuestion = document.createElement("div");
+    divQuestion.innerHTML = marked(text);
+    panelQuestions.appendChild(divQuestion);
+  });
+  fetchText(exercise.response, function (text) {
+    var divResponse = document.createElement("div");
+    divResponse.innerHTML = marked(text);
+    panelResponses.appendChild(divResponse);
+  });
+}
+
+function renderChapter(chapter) {
+  var mainAbstract = document.getElementById("abstract");
+  mainAbstract.innerHTML = `
+  <div id="container_abstract"></div>
+  
+  <div id="container_exercises">
+  <div id="panel_questions"></div>
+  <div id="panel_responses"></div>
+  </div>
+  `;
+
+  var containerAbstract = document.getElementById("container_abstract");
+  var containerExercises = document.getElementById("container_exercises");
+  var panelQuestions = document.getElementById("panel_questions");
+  var panelResponses = document.getElementById("panel_responses");
+
+  if (!chapter.abstract) {
+    return;
+  } else {
+    renderAbstract(chapter.abstract, containerAbstract);
+  }
+
+  if (!chapter.exercises) {
+    return;
+  } else {
+    createElement("", "hr", containerExercises, panelQuestions);
+    createElement("Exercises", "h2", containerExercises, panelQuestions);
+    for (let i = 0; i < chapter.exercises.length; i++) {
+      renderExercise(chapter.exercises[i], panelQuestions, panelResponses);
+    }
+  }
+}
+
 function renderChapters(listChapters) {
   var containerChapters = document.getElementById("chapters");
   containerChapters.innerHTML = "";
 
   for (let i = 0; i < listChapters.length; i++) {
-    let chapter = listChapters[i];
-
     var liChapter = document.createElement("li");
-    var linkChapter = createLinkChapter(chapter);
+    var linkChapter = createLinkChapter(listChapters[i]);
 
     linkChapter.onclick = function (e) {
       e.preventDefault();
       var isActive = toggleButton(this, containerChapters, "chapter_link");
 
       if (isActive) {
-        renderAbstract(chapter);
+        renderChapter(listChapters[i]);
       } else {
-        firstPageChapter();
+        resetPage();
       }
     };
 
     liChapter.appendChild(linkChapter);
     containerChapters.appendChild(liChapter);
-  }
-}
-
-function renderAbstract(chapter) {
-  var containerAbstract = document.getElementById("container_abstract");
-
-  if (!chapter.abstract) {
-    containerAbstract.innerHTML = "<h2>Not Available Yet</h2>";
-    return;
-  }
-
-  fetchText(chapter.abstract, function (text) {
-    containerAbstract.innerHTML = marked(text);
-  });
-  renderExercises(chapter.exercises);
-}
-
-function renderExercises(exercises) {
-  var containerExercises = document.getElementById("container_exercises");
-  var panelQuestions = document.getElementById("panel_questions");
-  var panelResponses = document.getElementById("panel_responses");
-
-  containerExercises.innerHTML = "";
-  panelQuestions.innerHTML = "";
-  panelResponses.innerHTML = "";
-
-  if (!exercises) {
-    containerExercises.innerHTML = "";
-    return;
-  }
-
-  var titleExercises = document.createElement("h2");
-  titleExercises.textContent = "Exercises";
-  containerExercises.appendChild(titleExercises);
-  containerExercises.appendChild(panelQuestions);
-  containerExercises.appendChild(panelResponses);
-
-  for (var i = 0; i < exercises.length; i++) {
-    fetchText(exercises[i].question, function (text) {
-      var divQuestion = document.createElement("div");
-      divQuestion.innerHTML = marked(text);
-      panelQuestions.appendChild(divQuestion);
-    });
-    fetchText(exercises[i].response, function (text) {
-      var divResponse = document.createElement("div");
-      divResponse.innerHTML = marked(text);
-      panelResponses.appendChild(divResponse);
-    });
   }
 }
 
